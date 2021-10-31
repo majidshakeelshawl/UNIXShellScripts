d21db6cac67179b0b8f855df43671c19100bce1c
Majid Shakeel Shawl <dev2.renosoftwares@gmail.com>
2021-10-31 18:54:51 +0530

    Variables in BASH

---
+++ Coursera/Google Python Automation/Week6/BashScripting/usingVariablesAndGlobs.sh
@@ -0,0 +1,7 @@
+line='-----------------------------'
+echo $date; echo $line
+echo $time; echo $line
+
+# Globs :  Globbing is mainly used to match filenames or searching for content in a file. 
+# Globbing uses wildcard characters to create the pattern.
+# ? single  * zero or more
