0d6aeb48f8682e4ffeae69e2e3c2ac2a6366fde8
Majid Shakeel <majidshakeelshawl@gmail.com>
2024-09-20 01:06:35 +0530

    feat: positional arguments in shell prog

---
+++ Personal/GeneralPrograms/positional_args.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+echo "The first positional argument is the name of the script \$0 $0"
+echo "Then others represent the respective arguments passed \$1 $1"
+echo "We also have a variable to get the number of arguments passed \$# $#"
